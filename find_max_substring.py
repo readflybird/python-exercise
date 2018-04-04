0884515d3158316fc3d296d841fdcfd12b7619df
readflybird <readflybird@163.com>
2018-04-04 11:42:47 +0800

    fix a bug

--- find_max_substring.py
+++ find_max_substring.py
@@ -1,3 +1,8 @@
+"""
+Given s string, Find max size of a sub-string, in which no duplicate chars present.
+"""
+
+
 def find_max_substring(str):
     max_size = 0
     l = []
@@ -12,11 +17,14 @@ def find_max_substring(str):
             else:
                 current_size += 1
                 l.append(e)
+    if current_size > max_size:
+        max_size = current_size
+
     return max_size
 
 
 if __name__ == "__main__":
-    #read usr input
-    str = raw_input("Please input a string\n")
-    max_size = find_max_substring(str)
-    print "The max substring has a size of %d in string: \"%s\"" % (max_size, str)
+    # read user input
+    user_input = raw_input("Please input a string\n")
+    max_size = find_max_substring(user_input)
+    print("The max substring has a size of %d in string: \"%s\"" % (max_size, user_input))
